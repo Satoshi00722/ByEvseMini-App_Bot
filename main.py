e154dbf34c84377af22655ba1a5cbe10247856c2
Satoshi00722 <hort6536@gmail.com>
2026-01-28 18:12:26 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import telebot
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise RuntimeError("❌ BOT_TOKEN не задан")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    text = (
+        "👋 Добро пожаловать в ByEvse Market\n\n"
+        "Мы создаём:\n"
+        "🤖 Telegram-ботов любой сложности\n"
+        "📱 Mini Apps под бизнес\n"
+        "🌐 Сайты и digital-решения\n"
+        "🎬 Видео и SMM-продвижение\n\n"
+        "💡 Работаем под ключ\n"
+        "🛡 Оплата только после проверки функционала\n\n"
+        "👇 Открой каталог и выбери услугу"
+    )
+    bot.send_message(message.chat.id, text)
+
+bot.infinity_polling()
